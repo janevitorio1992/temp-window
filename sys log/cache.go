fc4670d3cf94497b7b2cc3258f866ceac7afcf72
Jane Vitorio <154088631+janevitorio1992@users.noreply.github.com>
2023-12-17 21:11:31 +0200

    Create cache.go

---
+++ sys log/cache.go
@@ -0,0 +1,31 @@
+var testCasesDecomposition = []struct {
+	number   Input // given input as a decimal number
+	base     int   // given base to calculate from
+	expected []int //
+}
+
+func TestSmallestBasesWithEndBaseTooLow(t *testing.T) {
+	for _, test := range testCasesBaseTooLow {
+		observed, _ := test.number.smallesPalindBase(test.notHighEnoughEndBase)
+		if observed == test.expected {
+			t.Errorf("smallesPalindBase of %d returned %d, want 0",
+				test.number, observed)
+		}
+	}
+}
+
+func TestCreate(t *testing.T) {
+	preTestValidate(t)
+
+	net := network.New(t)
+	val := net.Validators[0]
+	ctx := val.ClientCtx
+	cli.ForceSkipConfirm = true
+
+  	t.Run("Recipe", func(t *testing.T) {
+		args := []string{"NewUser0", goodPLR}
+		cmd := DevCreate()
+		_, err := clitestutil.ExecTestCLICmd(ctx, cmd, args)
+		assert.Nil(t, err)
+	})
+}
